bf3bb8fde884326c314f8e861c1a3a19013a5ee5
rvalieris <rvalieris@users.noreply.github.com>
2020-11-22 17:00:28 -0300

    take 3

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,5 @@
-copy "%RECIPE_DIR%\build.sh" .
-FOR /F "delims=" %%i in ('cygpath.exe -u "%LIBRARY_PREFIX%"') DO set "PREFIX=%%i"
-FOR /F "delims=" %%i in ('cygpath.exe -u "%BUILD_PREFIX%"') DO set "BUILD_PREFIX=%%i"
-FOR /F "delims=" %%i in ('cygpath.exe -u "%SRC_DIR%"') DO set "SRC_DIR=%%i"
-FOR /F "delims=" %%i in ('cygpath.exe -u "%RECIPE_DIR%"') DO set "RECIPE_DIR=%%i"
 
-bash -lc "./configure --prefix=`cygpath -u $PREFIX` --bindir=`cygpath -u $LIBRARY_BIN` --libdir=`cygpath -u $LIBRARY_LIB`"
+bash -lc "./configure --prefix=`cygpath -u $PREFIX`"
 bash -lc "make"
 bash -lc "make install"
 
