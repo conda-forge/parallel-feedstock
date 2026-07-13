ace5cdb9f4c4b2fe6d0949fb91be97f481ad082b
rvalieris <rvalieris@users.noreply.github.com>
2026-07-13 14:06:21 -0300

    make use shebang use perl from the environ

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+# change shebang from the executables to use perl from the environ
+# to make sure it will use the perl from conda
+find src -type f -executable -exec sed -i 's|#!/usr/bin/perl|#!/usr/bin/env perl|g' '{}' \;
+
 ./configure --prefix="$PREFIX"
 make
 make check
